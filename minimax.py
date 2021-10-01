0c37cb78f162ff37e366621371216d239343fa8d
unknown <yarelit.mendoza@gmail.com>
2021-09-30 18:53:39 -0700

    none of this works at the moment 8( i hate recursion

--- minimax.py
+++ minimax.py
@@ -2,14 +2,16 @@ import math
 import copy
 #Yarelit Mendoza and Travis Chamness :)
 game = [1,2,5,2]
+treeDepth = math.log(len(game), 2)
+
 
 # function gets successors for current node
 def getSuccessors(state):
-    if len(state) == 1:
-        return None
-    else:
+    if len(state) > 1:
         child = copy.deepcopy(state)
         return [child[1:len(child)], child[0:len(child)-1]]
+    else:
+        return state
 
 #tests successors
 print("Parent node: ", game)
@@ -20,13 +22,28 @@ print("\nParent node: ", successors[0])
 a = getSuccessors(successors[0])
 print("Children: ", a)
 
+print("\nParent node: ", a[0])
+a2 = getSuccessors(a [0])
+print("Children: ", a2)
+
+def isTerminal(node):
+    if len(node) == 1:
+        return True
+
 # def value(state):
 # if the state is a terminal state: return the state's utility
-# if the next agent is MAX: return nax-value(state)
+# if the next agent is MAX: return max-value(state)
 # if the next agent is MIN: return min-value(state)
 
-def value(state):
-    return None
+def value(state, maxTurn):
+    if isTerminal(state):
+        return state[0]
+
+    if maxTurn:
+        return maxValue(state)
+    else:
+        return minValue(state)
+
 
 # def max-value(state):
 # initialize v = -inf
@@ -36,6 +53,13 @@ def value(state):
 
 def maxValue(state):
     v = -math.inf
+    successor = getSuccessors(state)
+
+    for i in range(len(successor)):
+         v = max(v, value(successor, False))
+    return v
+
+print(maxValue(a2[0]))
 
 # def min-value(state):
 # initialize v = +inf
@@ -45,4 +69,10 @@ def maxValue(state):
 
 def minValue(state):
     v = math.inf
+    successor =  getSuccessors(state)
+
+    for i in range(len(successor)):
+        v = min(v, value(successor, True))
+    return v
     
+value(game, True)
