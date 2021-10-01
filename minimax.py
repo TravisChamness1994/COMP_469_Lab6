d2b2c1a4b1f353aef8755c487009e5da19352764
unknown <yarelit.mendoza@gmail.com>
2021-09-30 18:08:07 -0700

    added skeleton as well

--- minimax.py
+++ minimax.py
@@ -1,21 +1,17 @@
 import math
 import copy
-#Yarelit Mendoza
+#Yarelit Mendoza and Travis Chamness :)
 game = [1,2,5,2]
-list = []
 
-# function gets successors for current node :)
+# function gets successors for current node
 def getSuccessors(state):
     if len(state) == 1:
         return None
     else:
-        child1 = copy.deepcopy(state)
-        child2 = copy.deepcopy(state)
-        
-        child1.pop(0)
-        child2.pop(-1)
-        return [child1, child2]
+        child = copy.deepcopy(state)
+        return [child[1:len(child)], child[0:len(child)-1]]
 
+#tests successors
 print("Parent node: ", game)
 successors = getSuccessors(game)
 print("Children: ", successors)
@@ -24,18 +20,13 @@ print("\nParent node: ", successors[0])
 a = getSuccessors(successors[0])
 print("Children: ", a)
 
-print("\nParent node:", a[0])
-c = getSuccessors(a[0])
-print("Children: ", c)
-
-print("\nParent node:", c[0])
-d = getSuccessors(c[0])
-print("Children: ", d)
-
 # def value(state):
 # if the state is a terminal state: return the state's utility
 # if the next agent is MAX: return nax-value(state)
 # if the next agent is MIN: return min-value(state)
+
+def value(state):
+    return None
 
 # def max-value(state):
 # initialize v = -inf
@@ -43,8 +34,15 @@ print("Children: ", d)
 # v = max(v, value(successor))
 # return v
 
+def maxValue(state):
+    v = -math.inf
+
 # def min-value(state):
 # initialize v = +inf
 # for each successor of state:
 # v = min(v, value(successor))
 # return v
+
+def minValue(state):
+    v = math.inf
+    
